73b76bf4dbebe85a1a613622acbc6c4be8afc7c4
Santiajo <ca.manzo@duocuc.cl>
2025-11-17 11:02:56 -0300

    Login y registro de usuarios con JWT

--- chatbot-backend/api/urls.py
+++ chatbot-backend/api/urls.py
@@ -3,11 +3,15 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from .views import *
 
 urlpatterns = [
-    # Esta es la ruta de LOGIN. Next.js enviará usuario/pass aquí
+    # Loguearse y obtener el tokens
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     
-    # Esta sirve para renovar el token sin loguearse de nuevo
+    # Renovar token sin loguearse de nuevo
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
+    # Registrar nuevo usuario
+    path('register/', RegisterView.as_view(), name='auth_register'),
 ]
