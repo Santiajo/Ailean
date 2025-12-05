57b64ea5c8e175c2444e4ef797667b4c56bffe7a
Santiajo <mail>
2025-12-05 11:23:56 -0300

    Coma en settings corregida y requirements actualizado

--- chatbot-backend/core/settings.py
+++ chatbot-backend/core/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
